124819d3c37e8b99b744879b296853cba1412f1a
vivek <vivek.vkit@gmail.com>
2018-08-04 23:13:01 +0530

    cricketers db - initial commit

--- app.js
+++ app.js
@@ -1,39 +1,98 @@
-const express = require('express');
-const app = express();
+const express = require('express');					// requiring locally running express server
+const app = express();								// using function returned by express
+const { cricketers } = require('./cricketers');		// requiring js data from other file via module exports
+const bodyParser = require('body-parser');			// requiring bodyParser to do POST & PUT
 
-const port = 3000;
+const port = 3000;									// using port 3000 to run the localhost server
 
-// log middleware
+// bodyParser - package middleware
+app.use(bodyParser.json())
+
+// logging - custom middleware
 app.use((req,res,next) => {
-	console.log(`${req.method} - ${req.url} - ${req.id} ${new Date()}`);
+	console.log(`${req.method} - ${req.url} - ${req.id} - ${new Date()}`);
 	next();
 })
 
 // route handlers
+// checking response --> TRUE
 app.get('/',(req,res) => {
 	res.send({
-		notice: "message need to be displayed here.."
+		notice: "you're ready to make requests"
 	});
 })
 
-app.post('/products',(req,res) => {
+// to get all the records from the db
+app.get('/cricketers',(req,res) => {
 	res.send({
-		"id": 1,
-		"name": "Virat",
-		"batting_avg" : 51.02
-	})
+		cricketers,
+		notice: "successfully obtained the records"
+	});
 })
 
-app.put('/products/:id',(req,res) => {
+// to get a single record from the db
+app.get('/cricketers/:id',(req,res) => {
+	let cricketer = cricketers.find((cricketer) => {
+		return cricketer.id == req.params.id;
+	})
+	if(cricketer) {
+		res.send({
+			cricketer,
+			notice: "successfully obtained the records"
+		});
+	} else {
+		res.send({
+			notice: `unable to retrieve the id ${req.params.id}`
+		});
+	}
+})
+
+// to insert a single record into the db
+app.post('/cricketers',(req,res) => {
+	let cricketer = req.body;
+	cricketers.push(cricketer);
 	res.send({
-		"name": "sachin"
+		cricketer,
+		notice: "successfully inserted the record"
+	});
+})
+
+// to update property(s) of a record in the db
+app.put('/cricketers/:id',(req,res) => {
+	let cricketer = cricketers.find((cricketer) => {
+		return cricketer.id == req.params.id
 	})
+	if(cricketer) {
+		cricketer.name = req.body.name;
+		res.send({
+			cricketer,
+			notice: "successfully updated the record"
+		});	
+	} else {
+		res.send({
+			notice: `unable to update the id ${req.params.id}`
+		});
+	}
 })
 
-app.delete('/products/id',(req,res) => {
-	res.send('The Record Has Been Successfully Deleted')
+// to delete a record from the db
+app.delete('/cricketers/:id',(req,res) => {
+	let index = cricketers.findIndex((cricketer) => {
+		return cricketer.id == req.params.id;
+	})
+	if(index > 0) {
+		cricketers.splice(index,1);
+		res.send({
+			notice: "successfully deleted the record"
+		});	
+	} else {
+		res.send({
+			notice: `unable to delete the id ${req.params.id}`
+		});
+	}
 })
 
+// localhost server listening to 3000
 app.listen(port,() => {
 	console.log(`Listening to the port at ${port}`)
 })
---
+++ cricketers.js
@@ -0,0 +1,21 @@
+// dummy db of cricketers
+let cricketers = [
+    {
+        id: 1,
+        name: "Virat",
+        batting_avg: 51.02
+    },{
+        id: 2,
+        name: "Sachin",
+        batting_avg: 48.08
+    },{
+        id: 3,
+        name: "Dhoni",
+        batting_avg: 88.08
+    }
+]
+
+// exporting the db modules to the app file
+module.exports = {
+    cricketers
+}
